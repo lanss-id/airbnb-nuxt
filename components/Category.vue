c98c58d3e98c152a9d925fab20cb7722bf7b6e88
lanss-id <maulanakayyis354@gmail.com>
2023-06-03 23:13:20 +0700

    category list

--- components/Category.vue
+++ components/Category.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="flex justify-between items-center space-x-6 mx-10 mt-4">
+    <nav class="flex justify-between items-center space-x-6 mx-10 mt-4 nav">
         <div class="flex space-x-4 truncate">
             <NuxtLink v-for="category in categories" :to="`/category/${category}`" class="grid place-items-center snap-x scroll-smooth">
                 <Icon name="carbon:hotel" size="1.5rem" />
@@ -15,7 +15,6 @@
 
 <script setup>
 let categories = []
-
 const getCategories = async () => {
     try {
         const res = await useFetch('https://dummyjson.com/products/categories')
@@ -24,38 +23,7 @@ const getCategories = async () => {
         console.log(error)
     }
 }
-
 categories = await getCategories()
-
-function scrollToNext() {
-    const scrollCategory = document.querySelector('.scroll-category')
-    scrollCategory.scrollLeft += scrollCategory.offsetWidth / 4
-}
-function scrollToPrev() {
-    const scrollCategory = document.querySelector('.scroll-category')
-    scrollCategory.scrollLeft -= scrollCategory.offsetWidth / 4
-}
-
-onMounted(() => {
-    if (process.client) {
-        const navScroll = () => {
-            const nav = document.querySelector('.nav-category')
-            if (document.documentElement.scrollTop > 175 || document.body.scrollTop > 175) {
-                nav.classList.add('border-b')
-                nav.classList.add('shadow-sm')
-            } else {
-                nav.classList.remove('border-b')
-                nav.classList.remove('shadow-sm')
-            }
-            window.addEventListener('scroll', navScroll)
-        }
-        navScroll()
-    }
-})
 </script>
 
-<style scope>
-.scroll-category::-webkit-scrollbar {
-    display: none;
-}
-</style>
+<style scope></style>
